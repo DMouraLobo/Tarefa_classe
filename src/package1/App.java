2f63d4512368a918f0734bf0fbb21762e89d5e69
DMouraLobo <diegomouralobo@outlook.com>
2025-01-05 20:24:37 -0300

    Commit Final
    Entrega da atividade

--- src/package1/App.java
+++ src/package1/App.java
@@ -3,8 +3,23 @@ package package1;
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		//Criação de objeto da classe carro
+		Carro carro1 = new Carro();
+		
+		//Atribuição de valores os atributos do objeto
+		carro1.setNumDePortas(4);
+		carro1.setAno(2010);
+		carro1.setCor("Prata");
+		carro1.setMarca("Honda");
+		carro1.setModelo("Civic");
+		
+		//Mostrando valores ao usúario utilizando os métodos get
+		System.out.println("Ano: " + carro1.getAno());
+		System.out.println("Cor: " + carro1.getCor());
+		System.out.println("Marca: " + carro1.getMarca());
+		System.out.println("Modelo: " + carro1.getModelo());
+		System.out.println("Número de portas: " + carro1.getNumDePortas());
 	}
 
 }
--- src/package1/Carro.java
+++ src/package1/Carro.java
@@ -1,11 +1,54 @@
 package package1;
 
 public class Carro {
-	
+
+	//Declaração das variaveis da classe
 	private int numDePortas;
 	private String marca;
 	private String modelo;
 	private String cor;
 	private int ano;
-	 
+	
+	//Inicio métodos getters and setters
+	public int getNumDePortas() {
+		return numDePortas;
+	}
+	
+	public void setNumDePortas(int numDePortas) {
+		this.numDePortas = numDePortas;
+	}
+	
+	public String getMarca() {
+		return marca;
+	}
+	
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	
+	public String getModelo() {
+		return modelo;
+	}
+	
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	
+	public String getCor() {
+		return cor;
+	}
+	
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+	
+	public int getAno() {
+		return ano;
+	}
+	
+	public void setAno(int ano) {
+		this.ano = ano;
+	}
+	//Fim dos métodos getters and setters 
+	
 }
